5b0f4e418d8181be168bd0fd95d51dccf806095c
Demi Lee <demilee.to@gmail.com>
2017-11-13 13:50:04 -0500

    working light switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 
+window.addEventListener("DOMContentLoaded", function() {
 
+  var status, onStatus, offStatus;
+
+  status = document.querySelector('.status');
+
+  onStatus = "It's too bright, turn the lights off!";
+  offStatus = "It's too dark, turn the lights on!";
+
+  status.innerHTML = onStatus
+
+  document.addEventListener("click", function(e) {
+    if (e.target.classList.contains("switch")) {
+      e.target.classList.toggle("on");
+      e.target.classList.toggle("off");
+      document.querySelector('body').classList.toggle("light");
+      document.querySelector('body').classList.toggle("dark");
+
+
+
+      if (status.innerHTML === onStatus) {
+        status.innerHTML = offStatus;
+      } else if (status.innerHTML === offStatus ) {
+        status.innerHTML = onStatus;
+      };
+
+    };
+  });
+
+});
